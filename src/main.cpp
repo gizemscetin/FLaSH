c45cfb32565aab3408a2b1e5d79c3c5c72f19796
yarkindoroz <yarkin@sabanciuniv.edu>
2016-03-24 10:53:09 -0400

    Upload FLaSH Files

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "homlib.h"
+
+using namespace std;
+using namespace NTL;
+
+
+
+int main()
+{
+
+    LaSH L;
+
+    ZZX m, b;
+
+    CipherText c, result;
+
+    m = 1;
+    L.Encrypt(c, m);
+
+    L.Mult(c, c);
+
+    L.Decrypt(b, c);
+    cout << b << endl;
+
+    return 0;
+}
