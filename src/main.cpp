7d84665c9b5d9677d3f21b9dd15b1cb6da307c97
gizemscetin <gizemscetin@gmail.com>
2017-03-22 16:24:12 -0400

    Update: main

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,6 @@ using namespace NTL;
 int main()
 {
     TestSort();
-/*
-    ZZX poly_mod;
-    PolyInit(poly_mod, 5, MonomialPlusOne);
-    cout << poly_mod << endl;
 
-    ZZX a(1);
-    a = a << 2;
-    cout << a << endl;
-    cout << a%poly_mod << endl;
-*/
     return 0;
 }
